bf4cd5d423d7b555dbfd3720d65c399838621915
r-okavv <ooxxbookxxoo@gmail.com>
2023-07-25 22:54:32 +0900

    Add:restaurants_controller and delete places_controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,7 @@ Rails.application.routes.draw do
   # get 'search', to: 'searches#search', as: 'search'
   # post 'search', to: 'searches#search'
   # get 'result', to: 'searches#result', as: 'result'
-  get 'index', to: 'places#index'
-  get 'search', to: 'places#search', as: 'search_places'
+  resources :restaurants, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
