71b0a6e9bd704e01ec955f28a68d9fa1adaef684
r-okavv <ooxxbookxxoo@gmail.com>
2023-07-29 23:59:09 +0900

    Add: 検索範囲入力欄を追加

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,20 @@
 class RestaurantsController < ApplicationController
   def index
+    @radius = params[:radius] || 50
     @client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
-    
+
     if params[:address].present?
       location = Geocoder.search(params[:address]).first
       if location
-        @restaurants = @client.spots(location.latitude, location.longitude, radius: 50, types: ['restaurant']).first(20)
+        @restaurants = @client.spots(location.latitude, location.longitude, @radius, types: ['restaurant']).first(20)
       else
         @restaurants = []
       end
     elsif params[:latitude].present? && params[:longitude].present?
       # Todo: paramsで直接parameterを渡さないよう処理する
-      @restaurants = @client.spots(params[:latitude], params[:longitude], radius: 100, types: ['restaurant']).first(20)
+      @restaurants = @client.spots(params[:latitude], params[:longitude], @radius, detail: true, types: ['restaurant']).first(20)
+      # @restaurants = @client.spots(params[:latitude], params[:longitude], radius: 100, types: ['restaurant']).first(20)
+      binding.pry
     else
       @restaurants = []
     end
