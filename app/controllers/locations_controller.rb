2ccf384148a7f1d7ced02e50ceab0fa8b010f0f7
r-okavv <ooxxbookxxoo@gmail.com>
2023-08-01 23:40:13 +0900

    Mod: 緯度、経度がhidden_fieldにセットされるように変更、取得した住所が日本語表示されるように変更

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,14 +1,14 @@
 class LocationsController < ApplicationController
     def get_location
-        latitude = params[:latitude]
-        longitude = params[:longitude]
-        address = ""
-    
-        if latitude.present? && longitude.present?
-          location = Geocoder.search([latitude, longitude]).first
-          address = location.address if location.present?
-        end
-    
-        render json: {address: address}
+    latitude = params[:latitude]
+    longitude = params[:longitude]
+    address = ""
+
+    if latitude.present? && longitude.present?
+      location = Geocoder.search([latitude, longitude], language: :ja).first
+      address = location.address if location.present?
     end
+
+    render json: {address: address}
+  end
 end
