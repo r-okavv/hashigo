ebbaed65eeaea7199f5a990cddb531f89ba94399
r-okavv <ooxxbookxxoo@gmail.com>
2023-08-01 21:55:54 +0900

    Mod: 入力住所から検索の場合に検索範囲を指定できるように変更

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,20 +1,21 @@
 class RestaurantsController < ApplicationController
   def index
-    @radius = params[:radius] || 50
     @client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
+    # binding.pry =>params[radius]はnil
+    radius = params[:radius] || 50
+    options = {radius: radius.to_i, types: ['restaurant']}
 
     if params[:address].present?
       location = Geocoder.search(params[:address]).first
       if location
-        @restaurants = @client.spots(location.latitude, location.longitude, @radius, types: ['restaurant']).first(20)
+        # @restaurants = @client.spots(location.latitude, location.longitude, @radius, types: ['restaurant']).first(20)
+        @restaurants = @client.spots(location.latitude, location.longitude,options).first(20)
       else
         @restaurants = []
       end
     elsif params[:latitude].present? && params[:longitude].present?
       # Todo: paramsで直接parameterを渡さないよう処理する
-      @restaurants = @client.spots(params[:latitude], params[:longitude], @radius, detail: true, types: ['restaurant']).first(20)
-      # @restaurants = @client.spots(params[:latitude], params[:longitude], radius: 100, types: ['restaurant']).first(20)
-      binding.pry
+      @restaurants = @client.spots(params[:latitude], params[:longitude], options).first(20)
     else
       @restaurants = []
     end
