69ad7cced9bf21d2bcc34ed3186c93ecde92a59a
r-okavv <ooxxbookxxoo@gmail.com>
2023-08-01 23:17:14 +0900

    Add: 現在地を住所に変換するためのLocationコントローラを追加

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,21 +1,18 @@
 class RestaurantsController < ApplicationController
   def index
     @client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
-    # binding.pry =>params[radius]はnil
     radius = params[:radius] || 50
     options = {radius: radius.to_i, types: ['restaurant']}
 
-    if params[:address].present?
+    if params[:latitude].present? && params[:longitude].present?
+      @restaurants = @client.spots(params[:latitude], params[:longitude], options).first(20)
+    elsif params[:address].present?
       location = Geocoder.search(params[:address]).first
       if location
-        # @restaurants = @client.spots(location.latitude, location.longitude, @radius, types: ['restaurant']).first(20)
         @restaurants = @client.spots(location.latitude, location.longitude,options).first(20)
       else
         @restaurants = []
       end
-    elsif params[:latitude].present? && params[:longitude].present?
-      # Todo: paramsで直接parameterを渡さないよう処理する
-      @restaurants = @client.spots(params[:latitude], params[:longitude], options).first(20)
     else
       @restaurants = []
     end
