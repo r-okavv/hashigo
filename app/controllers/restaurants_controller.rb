c8ecf82b7d8aeb79c3203ae0657e409fb6f0bddb
r-okavv <ooxxbookxxoo@gmail.com>
2023-08-02 16:21:30 +0900

    Add: クチコミスコアと店舗タイプのフィルタリングを追加

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,7 +2,9 @@ class RestaurantsController < ApplicationController
   def index
     @client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
     radius = params[:radius] || 50
-    options = {radius: radius.to_i, types: ['restaurant']}
+    # options = {radius: radius.to_i, types: params[:place_type],min_rating: params[:rating],closing_time: params[:closing_time]}
+    options = {radius: radius.to_i, types: params[:place_type],min_rating: params[:rating],closing_time: params[:closing_time]}
+    binding.pry
 
     if params[:latitude].present? && params[:longitude].present?
       @restaurants = @client.spots(params[:latitude], params[:longitude], options).first(20)
