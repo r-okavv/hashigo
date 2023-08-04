711e42d71cf179f4fd4e248b2563ce4ede6e261d
r-okavv <ooxxbookxxoo@gmail.com>
2023-08-04 14:27:08 +0900

    Mod: クチコミスコア入力フォームを修正

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,7 +4,6 @@ class RestaurantsController < ApplicationController
     radius = params[:radius] || 50
     # options = {radius: radius.to_i, types: params[:place_type],min_rating: params[:rating],closing_time: params[:closing_time]}
     options = {radius: radius.to_i, types: params[:place_type],min_rating: params[:rating],closing_time: params[:closing_time]}
-    binding.pry
 
     if params[:latitude].present? && params[:longitude].present?
       @restaurants = @client.spots(params[:latitude], params[:longitude], options).first(20)
