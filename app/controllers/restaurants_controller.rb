ce36b9165508c5a3a1ee3d83ada14a00d0db5117
r-okavv <ooxxbookxxoo@gmail.com>
2023-08-08 23:19:08 +0900

    Add: price_levelを追加

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,16 @@
 class RestaurantsController < ApplicationController
   def index
-    @client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
     radius = params[:radius] || 50
     # options = {radius: radius.to_i, types: params[:place_type],min_rating: params[:rating],closing_time: params[:closing_time]}
     options = {language: 'ja', radius: radius.to_i, types: params[:place_type],min_rating: params[:rating],closing_time: params[:closing_time], detail:true}
     # Todo: 日本語データのみ取得できるようにする。
+    @client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
 
     if params[:latitude].present? && params[:longitude].present?
       places_data = @client.spots(params[:latitude], params[:longitude], options).first(20)
     elsif params[:address].present?
       location = Geocoder.search(params[:address]).first
       if location
-        binding.pry
         places_data = @client.spots(location.latitude, location.longitude, options).first(20)
       else
         places_data = []
@@ -23,9 +22,11 @@ class RestaurantsController < ApplicationController
     @restaurants = places_data.map do |place_data|
       restaurant = Restaurant.find_or_initialize_by(place_id: place_data.place_id)
       unless restaurant.persisted?
+
         if place_data.photos.present?
           photo = place_data.photos.first
-          photo_url = @client.spot_photo_url(photo.photo_reference, maxwidth: 400, maxheight: 300)
+          photo_url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=#{photo.photo_reference}&key=#{ENV['GOOGLE_API_KEY']}"
+          html_attributions = photo.html_attributions.first
         end
 
         restaurant.attributes = {
@@ -37,10 +38,13 @@ class RestaurantsController < ApplicationController
           rating: place_data.rating,
           phone_number: place_data.formatted_phone_number,
           categories: place_data.types,
-          image_url: place_data.photos.present? ? @client.spot_photo_url(reference: place_data.photos.first.photo_reference, maxwidth: 400) : nil,
-          html_attributions: place_data.photos.first.html_attributions,
+          price_level: place_data.price_level,
+          # image_url: place_data.photos.present? ? @client.spot_photo_url(reference: place_data.photos.first.photo_reference, maxwidth: 400) : nil,
+          image_url: photo_url,
+          # html_attributions: place_data.photos.first.html_attributions,
+          html_attributions: html_attributions,
           url: place_data.url,
-          opening_hours: place_data.opening_hours ? place_data.opening_hours.weekday_text.join(", ") : "N/A"
+          opening_hours: place_data.opening_hours ? place_data.opening_hours["weekday_text"].join(", ") : "N/A"
         }
         restaurant.save
       end
