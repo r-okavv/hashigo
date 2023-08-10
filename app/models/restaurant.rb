aa36728fde4358d0bde48316c46570f06948688e
r-okavv <ooxxbookxxoo@gmail.com>
2023-08-10 22:45:30 +0900

    Add: Bookmarks Table

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ApplicationRecord
+    has_many :bookmarks
+    has_many :users_who_bookmarked, through: :bookmarks, source: :user
+    
     validates :place_id, presence: true
     validates :name, presence: true
     validates :latitude, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
-
+  has_many :bookmarks
+  has_many :bookmarked_restaurants, through: :bookmarks, source: :restaurant
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true
+
 end
