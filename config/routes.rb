d092c6744a70ea8805228733ab91a7b41860ea9e
r-okavv <ooxxbookxxoo@gmail.com>
2023-08-12 17:46:44 +0900

    Add: ViewにBookmarkボタンを追加、Associationの修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
   resources :restaurants, only: %i[index show] do
-    resource :bookmark, only: [:create, :destroy]
   end
   post 'get_location', to: 'locations#get_location'
+  resources :bookmarks, only: %i[create destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
