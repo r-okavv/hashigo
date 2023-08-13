d95d8bf3728cc518d32be30b9941858678bbd53b
r-okavv <ooxxbookxxoo@gmail.com>
2023-08-13 17:42:36 +0900

    Add: ブックマーク機能をTurboで非同期化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,19 @@
 class BookmarksController < ApplicationController
     def create
-        restaurant = Restaurant.find(params[:restaurant_id])
-        current_user.bookmark(restaurant)
-        redirect_back fallback_location: root_path
+        @restaurant = Restaurant.find(params[:restaurant_id])
+        current_user.bookmark(@restaurant)
+        respond_to do |format|
+          format.turbo_stream
+          format.html { redirect_back fallback_location: root_path }
+        end
       end
 
       def destroy
-        restaurant = current_user.bookmarks.find(params[:id]).restaurant
-        current_user.unbookmark(restaurant)
-        redirect_back fallback_location: root_path
+        @restaurant = current_user.bookmarks.find(params[:id]).restaurant
+        current_user.unbookmark(@restaurant)
+        respond_to do |format|
+          format.turbo_stream
+          format.html { redirect_back fallback_location: root_path }
+        end
       end
 end
