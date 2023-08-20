c0dc828312fa4c84ea56f34f0cd89ab5653761fd
r-okavv <ooxxbookxxoo@gmail.com>
2023-08-20 16:07:12 +0900

    Mod:user modelのassociationにdependencyを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
   has_many :bookmark_restaurants, through: :bookmarks, source: :restaurant
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
