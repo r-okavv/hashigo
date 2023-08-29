633b0774a146a952d263232f68a8e202e178524c
r-okavv <ooxxbookxxoo@gmail.com>
2023-08-29 23:58:54 +0900

    Mod:find_or_create_from_api_data(data)メソッドを修正、fetch_place_details(place_id)を追加

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,10 @@
 class RestaurantsController < ApplicationController
+  require 'net/http'
+  require 'uri'
+  require 'json'
+
   skip_before_action :require_login, only: %i[index show]
-  before_action :set_google_client
+  # before_action :set_google_client
   before_action :set_restaurant, only: [:tags, :add_tag, :remove_tag, :update_tags]
 
   def index
@@ -61,9 +65,9 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
   end
 
-  def set_google_client
-    @client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
-  end
+  # def set_google_client
+  #   @client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
+  # end
 
   def search_params
     params.permit(:radius, :place_type, :rating, :closing_time, :latitude, :longitude, :address)
@@ -73,20 +77,51 @@ class RestaurantsController < ApplicationController
     Restaurant.find_or_create_from_api_data(place_data)
   end
 
+  # def fetch_restaurants
+  #   places_data = if params[:latitude].present? && params[:longitude].present?
+  #                   @client.spots(params[:latitude], params[:longitude], search_options).first(20)
+  #                 else
+  #                   []
+  #                 end
+
+  #   @restaurants = places_data.map { |place_data| find_or_create_restaurant(place_data) }
+
+  #   # rating パラメータが存在する場合、その値以上の評価を持つレストランのみをフィルタリング
+  #   if search_params[:rating].present?
+  #     @restaurants = @restaurants.select { |restaurant| restaurant.rating && restaurant.rating >= search_params[:rating].to_f }
+  #   end
+
+  #   @restaurants
+  # end
+
   def fetch_restaurants
-    places_data = if params[:latitude].present? && params[:longitude].present?
-                    @client.spots(params[:latitude], params[:longitude], search_options).first(20)
-                  else
-                    []
-                  end
-
-    @restaurants = places_data.map { |place_data| find_or_create_restaurant(place_data) }
-
+    # NearBySearchのエンドポイントURL
+    base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+  
+    # リクエストのパラメータを設定
+    parameters = {
+      location: "#{params[:latitude]},#{params[:longitude]}",
+      radius: search_params[:radius] || 50,
+      type: search_params[:place_type],
+      key: ENV['GOOGLE_API_KEY'],
+      language: 'ja'
+    }
+  
+    # パラメータをURLにエンコードして結合
+    url = base_url + parameters.to_query
+  
+    # HTTPリクエストを行い、レスポンスを取得
+    response = Net::HTTP.get(URI(url))
+    results = JSON.parse(response)["results"]
+  
+    # レスポンスからレストランのデータを取得または作成
+    @restaurants = results.map { |place_data| find_or_create_restaurant(place_data) }
+  
     # rating パラメータが存在する場合、その値以上の評価を持つレストランのみをフィルタリング
     if search_params[:rating].present?
       @restaurants = @restaurants.select { |restaurant| restaurant.rating && restaurant.rating >= search_params[:rating].to_f }
     end
-
+  
     @restaurants
   end
 
@@ -107,19 +142,19 @@ class RestaurantsController < ApplicationController
     @restaurants
   end
 
-  def search_options
-    radius = params[:radius] || 50
-    {
-      language: 'ja',
-      radius: radius.to_i,
-      types: params[:place_type],
-      closing_time: params[:closing_time],
-      detail: true
-    }
-  end
+  # def search_options
+  #   radius = params[:radius] || 50
+  #   {
+  #     language: 'ja',
+  #     radius: radius.to_i,
+  #     types: params[:place_type],
+  #     closing_time: params[:closing_time],
+  #     detail: true
+  #   }
+  # end
 
-  def search_params
-    params.permit(:radius, :place_type, :rating, :closing_time, :latitude, :longitude, :address)
-  end
+  # def search_params
+  #   params.permit(:radius, :place_type, :rating, :closing_time, :latitude, :longitude, :address)
+  # end
 
 end
