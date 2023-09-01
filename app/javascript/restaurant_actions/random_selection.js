dc33f207a398510f7e06ee1e1557d51274960a73
r-okavv <ooxxbookxxoo@gmail.com>
2023-09-02 04:21:36 +0900

    Mod: Javascriptのメソッドを別ファイルに切り分け、address_searchから不要なコードを削除

---
+++ app/javascript/restaurant_actions/random_selection.js
@@ -0,0 +1,37 @@
+document.addEventListener('turbo:load', function() {
+  const selectCandidateButton = document.getElementById('select-candidate-button');
+  const instruction = document.getElementById('instruction');
+  const randomButton = document.getElementById('random-button');
+  const checkboxes = document.querySelectorAll('.restaurant-checkbox');
+
+  if (selectCandidateButton && instruction && randomButton && checkboxes.length > 0) {
+    selectCandidateButton.addEventListener('click', function() {
+      checkboxes.forEach(checkbox => {
+        checkbox.classList.remove('hidden');
+      });
+      instruction.classList.remove('hidden'); 
+      selectCandidateButton.classList.add('hidden');
+      randomButton.classList.remove('hidden');
+    });
+
+    randomButton.addEventListener('click', function() {
+      // 選択されたレストランのIDを取得
+      const selectedIds = [];
+      checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+          selectedIds.push(checkbox.getAttribute('data-restaurant-id'));
+        }
+      });
+
+      // IDのリストからランダムに1つ選択
+      const randomId = selectedIds[Math.floor(Math.random() * selectedIds.length)];
+
+      // 選択されたレストランの詳細ページにリダイレクト
+      if (randomId) {
+        window.location.href = `/restaurants/${randomId}`;
+      } else {
+        alert('レストランを選択してください');
+      }
+    });
+  }
+});
