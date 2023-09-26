c138df35f7866c25e33faa54758822e2b6fefa2b
r-okavv <ooxxbookxxoo@gmail.com>
2023-09-26 21:50:00 +0900

    Add:oauths_controllerを追加

---
+++ app/controllers/oauths_controller.rb
@@ -0,0 +1,24 @@
+class OauthsController < ApplicationController
+  skip_before_action: :require_login
+  def oauth
+    login_at(auth_params[:provider])
+  end
+
+  def callback
+    provider = auth_params[:provider]
+    if (@user = login_form(provider))
+      redirect_to search_restaurants_path, notice:"#{provider.titleize}でログインしました"
+    else
+      @user = create_form(provider)
+      reset_session
+      auto_login(@user)
+      redirect_to search_restaurants_path, notice:"#{provider.titleize}でログインしました"
+    end
+  end
+
+  private
+
+  def auth_params
+    params.permit(:code, :provider)
+  end
+end
