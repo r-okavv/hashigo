314e0df0f874b2f380c071e968276d0c8de3a778
r-okavv <ooxxbookxxoo@gmail.com>
2023-09-27 18:50:18 +0900

    Add:config/settingsにgoogle_callback_urlを追加

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -163,7 +163,7 @@ Rails.application.config.sorcery.configure do |config|
   #
   config.google.key = Rails.application.credentials.dig(:google, :client_id)
   config.google.secret = Rails.application.credentials.dig(:google, :client_secret)
-  config.google.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=google"
+  config.google.callback_url = Settings.sorcery[:google_callback_url]
   # config.google.user_info_mapping = {:email => "email", :username => "name"}
   # config.google.scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
   #
