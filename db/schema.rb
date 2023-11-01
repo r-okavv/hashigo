e78e7fd29381018b58377f08df1d10fca4d73e09
r-okavv <ooxxbookxxoo@gmail.com>
2023-11-01 23:51:53 +0900

    Add:reset_passwordモジュールの導入、migrationの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_26_115715) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_01_144945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -114,7 +114,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_26_115715) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
   create_table "votes", force: :cascade do |t|
