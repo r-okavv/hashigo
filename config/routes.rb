25703fb25990e3cac31b3465b53aa0e5d9c79a39
r-okavv <ooxxbookxxoo@gmail.com>
2023-11-02 00:44:06 +0900

    Add:password_resets_controllerを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/create'
+  get 'password_resets/edit'
+  get 'password_resets/update'
   root 'static_pages#top'
   # root 'restaurants#index'
   get 'login' => 'user_sessions#new', :as => :login
