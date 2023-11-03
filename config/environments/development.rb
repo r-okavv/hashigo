4ba2ba4abe5146241db05fef5bbd0b23ff6fd56d
r-okavv <ooxxbookxxoo@gmail.com>
2023-11-03 11:24:52 +0900

    Add:gmailのuser_nameとpasswordを追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,4 +79,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
 end
